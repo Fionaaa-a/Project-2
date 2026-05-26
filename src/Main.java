3a476cf3aae4581f3bb5a6f45054a913249506bc
user <user@10.127.33.249>
2026-05-26 09:28:03 -0400

    fiona adds

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.FileWriter;
+import java.io.File;
+import java.util.Scanner;
 /**
  *
  * @author user
@@ -15,51 +19,54 @@ public class Main extends javax.swing.JFrame {
      */
     public Main() {
         initComponents();
-        this.setVisible(true);
-        cases [0] = new PrivacyCase(
-            "The Always-On Microphone",            
-            "A smart speaker company recorded household conversations even when the device was not activated. Employees reviewed the recordings.",
-            "Privacy Case"
-        );
-        cases [1] = new AlgorithmCase(
-            "The Biased Hiring Bot",
-            "A tech company's AI screening tool ranked male applicants higher than equally qualified female applicants.",
-            "Algorithm Case"
-        );
-        cases [2] = new MisinformationCase(
-            "The Deepfake Politician",
-            "A deepfake video of a candidate saying things they never said spread widely online during an election before being identified as fake.",
-            "Misinformation Case"
-
-        );
-        cases [3] = new IntellectualPropertyCase(
-            "AI Trained on Artist Work",
-            "An AI image generator was trained on millions of artworks scraped without permission. Artists receive no credit or payment.",
-            "Intellectual Property Case"
-        );
-        cases [4] = new PrivacyCase(
-            "The Constant Tracker",
-            "The company of a weather app software was found to be aware of users exact location even when they did not have the app open.",
-            "Privacy Case"        
-        );
-        cases [5] = new AlgorithmCase(
-            "The Judge",
-            "An AI model utilized in courthouses for the purpose of distinguishing between faces was trained using mostly light-skinned individuals.",
-            "Algorithm Case"
         
-        );
-        cases [6] = new MisinformationCase(
-            "The Deepfake Doctor",
-            "A deepfake video of a well-known doctor promoting a capsule spread widely online before being identified as fake.",
-            "Misinformation Case"
-        );
-        cases [7] = new IntellectualPropertyCase(
-            "Stealing art work",
-            "A company used an artist’s drawing for advertising purpose without asking for permission.",
-            "Intellectual Property Case"
-        );
+//        this.setVisible(true);
+        int line=0;
+        try{
+            
+            Scanner fileInput = new Scanner(new File("user_database.txt"));
+            
+//            Scanner fileInput = new Scanner(new File("goog.txt"));
+            while(fileInput.hasNext()){
+                 
+                String output = fileInput.nextLine();
+                String [] info = output.split(",");
+                String title = info[0].trim();
+                String description = info[1].trim();
+                String category = info[2].trim();
+                if (line==0){
+                    cases[0]=new PrivacyCase(title, description, category);
+                   
+                }else if(line==1){
+                    cases [1] = new AlgorithmCase(title, description, category);
+                }else if(line==2){
+                    cases [2] = new MisinformationCase(title, description, category);
+                }else if (line==3){
+                    cases [3] = new IntellectualPropertyCase(title, description, category);
+                }else if (line==4){
+                    cases[4]=new PrivacyCase(title, description, category);
+                }else if(line==5){
+                    cases [5] = new AlgorithmCase(title, description, category);
+                }else if(line==6){
+                    cases [6] = new MisinformationCase(title, description, category);
+                }else if (line==7){
+                    cases [7] = new IntellectualPropertyCase(title, description, category);
+                }
+                line++;
+            }
+            fileInput.close();
+        }catch(IOException e){
+            System.err.println("Error");
+          
+            
+        }
+       
 
+        
+        
     }
+    
+    
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -109,7 +116,7 @@ public class Main extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(193, 193, 193)
+                .addGap(159, 159, 159)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel2)
                     .addComponent(jLabel1)
@@ -121,12 +128,12 @@ public class Main extends javax.swing.JFrame {
                             .addComponent(jLabel5)
                             .addComponent(jLabel6)
                             .addComponent(jButton1))))
-                .addContainerGap(226, Short.MAX_VALUE))
+                .addContainerGap(170, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(61, 61, 61)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(65, Short.MAX_VALUE)
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jLabel2)
@@ -140,7 +147,7 @@ public class Main extends javax.swing.JFrame {
                 .addComponent(jLabel6)
                 .addGap(29, 29, 29)
                 .addComponent(jButton1)
-                .addContainerGap(91, Short.MAX_VALUE))
+                .addGap(62, 62, 62))
         );
 
         pack();
@@ -181,7 +188,8 @@ public class Main extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-
+        
+        
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
